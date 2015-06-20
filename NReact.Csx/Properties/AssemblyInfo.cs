ec2723c10ac280cef1178ac9a4a642424088260f
Lex Lavnikov <lex@lavnikov.com>
2015-06-21 01:06:22 +0200

    Update AssemblyInfo.cs

--- NReact.Csx/Properties/AssemblyInfo.cs
+++ NReact.Csx/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NReact.Csx")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Lex Lavnikov 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
